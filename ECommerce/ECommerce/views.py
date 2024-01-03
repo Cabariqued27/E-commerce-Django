58eb0208fcf64ad8378e2f7819a7f6b55f4d3349
Cabariqued27 <felipe27th@gmail.com>
2024-01-03 16:48:48 -0500

    static Ready

--- ECommerce/ECommerce/views.py
+++ ECommerce/ECommerce/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html',{
-        #context
+        'message': 'Listado de productos',
+        'title':'D´Mare',
+        'products':[
+            {'title':'playera','price':5,'stock':True},
+            {'title':'playera2','price':15,'stock':True},
+            {'title':'playera3','price':20,'stock':False}
+        ]
     })
