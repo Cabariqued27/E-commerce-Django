79a13a973d40215643a4184739451b6e6e0fbcee
Cabariqued27 <felipe27th@gmail.com>
2024-01-30 22:28:43 -0500

    total ready

--- ECommerce/carts/models.py
+++ ECommerce/carts/models.py
@@ -1,6 +1,7 @@
+import decimal
 import uuid
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, m2m_changed
 from users.models import User
 from products.models import Product
 # Create your models here.
@@ -12,12 +13,32 @@ class Cart(models.Model):
     subtotal = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
     total = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add= True)  
+    FEE = 0.05
 
     def __str__(self):
         return self.cart_id
+    
+    def update_totals(self):
+        self.update_subtotals()
+        self.update_total()
+    
+    def update_subtotals(self):
+        self.subtotal = sum([product.price for product in self.products.all()]) 
+        self.save()
+
+    def update_total(self):
+        self.total = self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE))
+        self.save()
+
     
 def set_cart_id(sender, instance, *args, **kwargs):
     if not instance.cart_id:
         instance.cart_id= str(uuid.uuid4())
 
+def update_totals(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        instance.update_totals()
+
+
 pre_save.connect(set_cart_id, sender=Cart)
+m2m_changed.connect(update_totals, sender=Cart.products.through)
